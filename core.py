fb75bd6ee631d20d770dc795c3697f997f66bd60
hunk6576 <hunk6576@gmail.com>
2025-04-17 20:53:58 +0200

    Update core.py

--- core.py
+++ core.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 async def ask_gpt(prompt, system_prompt=None):
     try:
